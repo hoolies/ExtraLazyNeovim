ba34e7f46995ea290637a553ae0cefdd53e396c9
hoolies <c.rouvellas@gmail.com>
2025-02-12 20:38:50 -0500

    Adding my settings to LazyVim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,55 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-------------------------------------------------- AUTOCMDS --------------------------------------
+
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
+-- Telescope when you run nvim without a file as an argument
+-- autocmd("VimEnter", {
+--   callback = function()
+--     if vim.fn.argv(0) == "" then
+--       require("telescope.builtin").oldfiles()
+--     elseif vim.fn.isdirectory(vim.v.argv[2]) == 1 then
+--       vim.api.nvim_set_current_dir(vim.v.argv[2])
+--     end
+--   end,
+-- })
+
+-- Remove whitespace on save
+-- autocmd({
+--   "BufWritePre",
+--   "BufEnter",
+-- }, {
+--   pattern = "",
+--   command = ":%s/\\s\\+$//e",
+-- })
+
+-- Adding my custom colours
+autocmd("Vimenter", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#29465B" })
+    vim.api.nvim_set_hl(0, "CursorColumn", { fg = "#737CA1" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FF00FF" })
+    vim.api.nvim_set_hl(0, "IncSearch", { bg = "#FF00FF" })
+  end,
+})
+
+-- Don't auto commenting new lines
+autocmd("BufEnter", {
+  pattern = "",
+  command = "set fo-=c fo-=r fo-=o",
+})
+
+-- You open the file at the point you where last
+autocmd("BufReadPost", { command = "normal! g'\"" })
+
+-- Turn off relative numbers to terminal
+-- autocmd("TermOpen", { command = "setlocal listchars= nonumber norelativenumber" })
+
+-- Turn off relative numbers to oil buffers
+-- autocmd("FileType", {
+--   pattern = "oil",
+--   callback = function()
+--     vim.wo.number = false
+--     vim.wo.relativenumber = false
+--   end,
+-- })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,56 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-----------------------------------------------------------
+-- Define keymaps of Neovim and installed plugins.
+-----------------------------------------------------------
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Change leader to spacebar
+map("", "<SPACE>", "<nop>")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
+
+-- Disable arrow keys
+map("n", "<up>", "<nop>")
+map("n", "<down>", "<nop>")
+map("n", "<left>", "<nop>")
+map("n", "<right>", "<nop>")
+
+-- Better Navigation, it is not required as long as scrolloff = 999
+-- map('n', 'j', 'jzz')
+-- map('n', 'k', 'kzz')
+
+-- Navigate in Insert Mode
+map("i", "<C-k>", "<Up>")
+map("i", "<C-j>", "<Down>")
+map("i", "<C-h>", "<Left>")
+map("i", "<C-l>", "<Right>")
+map("i", "jj", "<Esc>")
+
+-- Toggle the undotree
+map("n", "<leader>U", ":UndotreeToggle<CR>", { desc = "Opens UndoTree" })
+
+-- Clear search with <esc>
+map("n", "<Esc>", "<cmd>nohl<cr>", { desc = "Escape and Clear hlsearch" })
+
+-- Oil
+-- map("n", "<leader>e", ":Oil --float<CR>", { desc = "Opens Oil in floating mode" })
+
+-- Replace the world under the cursor
+map("n", "<Leader>/", [[:%s/<C-r><C-w>//g<Left><Left>]])
+map("v", "<Leader>/", [[:%s/<C-r><C-w>//g<Left><Left>]])
+
+-- Delete without copying into register
+map("n", "x", '"_x')
+map("v", "x", '"_x')
+map("n", "dd", '"_dd')
+map("n", "dd", '"_dd')
